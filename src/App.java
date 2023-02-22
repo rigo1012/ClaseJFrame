67adf1050cbcddf6ef5632cbb3c202e103dd1c29
rigo1012 <rodrigodomingo764@gmail.com>
2023-02-21 21:56:56 -0600

    Actualizacion correcta

---
+++ src/App.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JFrame {
+
+    public App(){
+        setLayout(null);
+        setSize(500,500);
+        setVisible(true);
+        //pestañas
+
+        JTabbedPane pestañas = new JTabbedPane();
+        // Pneles
+
+        JPanel pantalla1= new JPanel();
+        //Carateristicas
+        pantalla1.setBackground(Color.lightGray);
+
+        JPanel pantalla2= new JPanel();
+        pantalla2.setBackground(Color.cyan);
+
+        //Se agregan como pestañas
+
+        pestañas.addTab("Vista",pantalla1);
+        pestañas.addTab("Vista 2", pantalla2);
+
+        add(pestañas);
+        pestañas.setBounds(0,0,280,300);
+    }
+    public static void main (String[] args){
+        App app = new App();
+    }
+    }
